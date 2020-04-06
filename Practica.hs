5553386eddff8958ad19105dea37a56a553a229a
Arian Huapaya <55849001+ArianH23@users.noreply.github.com>
2020-04-06 19:14:26 +0200

    Add files via upload

---
+++ Practica.hs
@@ -0,0 +1,27 @@
+-- createBoard :: String -> String -> [[[Char]]]
+createBoard n m = splitEvery y (take (x * y) rows)
+    where x = read n :: Int
+          y = read m :: Int
+rows = ['_'] ++ rows
+
+
+-- splitEvery :: Int -> [[Char]] -> [[[Char]]]
+splitEvery _ [] = []
+splitEvery n list = first : (splitEvery n rest)
+  where
+    (first,rest) = splitAt n list
+
+
+
+main = do 
+    putStrLn "Elige el numero de filas del tablero"
+    n <- getLine
+    putStrLn "Elige el numero de columnas del tablero"
+    m <- getLine
+    let b = createBoard n m
+    muestraBoard b
+
+muestraBoard [] = return()
+muestraBoard (b:bs) = do 
+    print b
+    muestraBoard bs
